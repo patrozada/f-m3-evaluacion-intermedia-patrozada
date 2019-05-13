94db1c6b68c7e1ab2aa8108495762bbcc2b91f0d
patrozada <patriciarozada@gmail.com>
2019-05-13 17:22:01 +0200

    Initial commit. Build docs

---
+++ docs/precache-manifest.8db1b99ddde787279b47b0cf9304bd07.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5d68373da4f403f4086e2b229042f9d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "79f56d3c7f2cf2e639d2",
+    "url": "./static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "./static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "79f56d3c7f2cf2e639d2",
+    "url": "./static/js/main.5b88173a.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="card">
+      <ul>
+        Hola!
+      </ul>
+    </div>
+  );
+}
+
+export default App;
